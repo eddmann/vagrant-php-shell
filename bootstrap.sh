53bb6e595700696dbe8f4c46c024152b78194479
Edward Mann <the@eddmann.com>
2013-11-29 17:29:21 +0000

    Added apache conf file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -61,14 +61,10 @@ sed -i "s/^\listen.*$/listen = \/tmp\/php5-fpm.sock/g" /etc/php-fpm.d/www.conf
 if [ -z $APACHE ]; then
 
 # nginx
-echo "[nginx]
-name=nginx repo
-baseurl=http://nginx.org/packages/centos/6/x86_64/
-gpgcheck=0
-enabled=1" > /etc/yum.repos.d/nginx.repo
+rpm -Uvh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
 yum install -y nginx
-rm /etc/nginx/conf.d/default.conf
-ln -fs /vagrant/nginx.conf /etc/nginx/conf.d/nginx.conf # use provided
+rm /etc/nginx/conf.d/*.conf
+ln -fs /vagrant/nginx.conf /etc/nginx/conf.d/nginx.conf
 chkconfig --levels 235 nginx on
 /etc/init.d/nginx start
 
@@ -80,28 +76,15 @@ else
 
 # apache
 yum install -y httpd
-rm -rf /var/www/html
-ln -fs /vagrant /var/www/html
-sed -i "s/^\s*DocumentRoot.*$/DocumentRoot \"\/var\/www\/html\/public\"/g" /etc/httpd/conf/httpd.conf
-sed -i "s/^\s*#ServerName.*$/ServerName development:80/g" /etc/httpd/conf/httpd.conf
-sed -i "s/^\s*AllowOverride.*$/AllowOverride All/g" /etc/httpd/conf/httpd.conf
-sed -i "s/^\s*#EnableSendfile.*$/EnableSendfile Off/g" /etc/httpd/conf/httpd.conf
-sed -i "s/^\s*DirectoryIndex.*$/DirectoryIndex index.php index.html/g" /etc/httpd/conf/httpd.conf
 chkconfig --levels 235 httpd on
 
 # mod_fastcgi
 rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 yum --enablerepo=rpmforge-extras install -y mod_fastcgi
-sed -i "s/^/#/" /etc/httpd/conf.d/php.conf # comment-out all php.conf
-sed -i "s/^\FastCgiWrapper.*$/FastCgiWrapper Off/g" /etc/httpd/conf.d/fastcgi.conf
-echo "<IfModule mod_fastcgi.c>
-AddHandler php5-fcgi .php
-Action php5-fcgi /php5-fcgi
-Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi
-FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi -socket /tmp/php5-fpm.sock -pass-header Authorization
-</IfModule>" >> /etc/httpd/conf.d/fastcgi.conf
-mkdir /usr/lib/cgi-bin/ # req. php-fpm
+mkdir /usr/lib/cgi-bin/
+rm /etc/httpd/conf.d/*.conf
 
+ln -fs /vagrant/apache.conf /etc/httpd/conf.d/apache.conf
 /etc/init.d/httpd start
 
 fi
